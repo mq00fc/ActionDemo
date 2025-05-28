e98dfed241d530e1648097bc0a4177238a74ff33
mq00fc <781187193@qq.com>
2025-05-28 12:14:32 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine($"this is a aot project demo by github actions {DateTime.Now.ToShortDateString()}");
             Console.WriteLine("这是一个测试的demo");
-            Console.ReadKey();
+            Console.ReadKey(); 
         }
     }
 }
